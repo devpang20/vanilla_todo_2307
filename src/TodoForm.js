d4f4f8dff975eef8abeac75b89cd20fb5f53b3bb
sik2 <devpang20@gmail.com>
2023-08-03 20:10:57 +0900

    TodoForm.js, TodoList.js 요구사항에 맞게 구조를 만든다.

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,5 +1,10 @@
 export default function TodoForm({$target}) {
-    const $form = document.createElement('h1');
-    $form.textContent = 'form';
+    const $form = document.createElement('form');
     $target.appendChild($form);
+
+    $form.innerHTML = `
+        <input type='text' name='todo'>
+        <button>입력</button>
+    `
+
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,12 @@
 export default function TodoList({$target}) {
-    const $list = document.createElement('h1');
-    $list.textContent = 'list';
+    const $list = document.createElement('div');
     $target.appendChild($list);
+
+    $list.innerHTML = `
+        <ul>
+            <li>item1</li>
+            <li>item2</li>
+            <li>item3</li>
+        </ul>
+    `
 }
