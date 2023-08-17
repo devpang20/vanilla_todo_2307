7d29cd796df73c518ec1094a5a202e9622457d75
sik2 <devpang20@gmail.com>
2023-08-17 17:10:26 +0900

    2nd

--- src/App.js
+++ src/App.js
@@ -4,19 +4,63 @@ import TodoList from "./TodoList.js";
 import { getItem, setItem } from "./storage.js";
 
 export default function App({$target}) {
-    const intialState = getItem('todos', []);
-    const header = new Header({$target});
+    const intialState = getItem('todos', [
+        {
+            id: 1,
+            text: "할일1",
+            checked: false
+        }, 
+        {
+            id: 2,
+            text: "할일2",
+            checked: false
+        }, 
+        {
+            id: 3,
+            text: "할일3",
+            checked: false
+        }
+    ]);
+
+    let currentId = intialState[intialState.length - 1].id;
+
+    new Header({$target});
+
+    const onSubmit = (text) => {
+        const nextState = todoList.state.concat([
+            {
+                id: currentId + 1,
+                text,
+                checked: false
+            }
+        ])
+        currentId++;
+        todoList.setState(nextState)
+        setItem('todos', JSON.stringify(nextState))
+    }
+
+    const onDelete = (id) => {
+        const nextState = todoList.state.filter(todo => todo.id !== id)
+        todoList.setState(nextState)
+    }
+
+    const onToggle = (id) => {
+        const nextState = todoList.state.map(todo => 
+            todo.id === id ? {...todo, checked: !todo.checked} : todo
+        )
+        todoList.setState(nextState)    
+    }
+
     new TodoForm({
         $target,
-        onSubmit: (text) => {
-            const nextState = [...todoList.state, text]
-            todoList.setState(nextState)
-            header.render();
-
-            setItem('todos', JSON.stringify(nextState))
-        }
+        onSubmit
     })
 
-    const todoList = new TodoList({$target, intialState})
+    const todoList = new TodoList({
+        $target, 
+        intialState, 
+        onDelete,
+        onToggle
+    })
 
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-export default function TodoList({$target, intialState}) {
+export default function TodoList({ $target, intialState, onDelete, onToggle }) {
     const $list = document.createElement('div');
     $target.appendChild($list);
 
@@ -8,11 +8,29 @@ export default function TodoList({$target, intialState}) {
         this.state = nextState
         this.render()
     }
-    
+
+    $list.addEventListener('click', e => {
+        if (e.target.classList.contains('delete-btn')) {
+            const id = parseInt(e.target.getAttribute('data-id'));
+            onDelete(id);
+        } else if (e.target.classList.contains('toggle-checkbox')) {
+            const id = parseInt(e.target.getAttribute('data-id'));
+            onToggle(id);
+        }
+    });
+
+
     this.render = () => {
+
         $list.innerHTML = `
             <ul>
-                ${this.state.map(text => `<li>${text}</li>`).join('')}
+                ${this.state.map(todo => `
+                <li style="text-decoration: ${todo.checked ? 'line-through' : 'none'}">
+                    <input type=checkbox ${todo.checked ? 'checked' : ''} class="toggle-checkbox" data-id="${todo.id}" />
+                    ${todo.id}
+                    <span>${todo.text}</span>
+                    <button class="delete-btn" data-id="${todo.id}">삭제</button>
+                </li>`).join('')}
             </ul>
         `
     }
