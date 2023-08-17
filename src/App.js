8f034fb41559d7cd857efb018487b8ff5680c6fb
sik2 <devpang20@gmail.com>
2023-08-17 20:51:47 +0900

    할일 체크 여부 구현

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import TodoList from "./TodoList.js";
 import { getItem, setItem } from "./storage.js";
 
 export default function App({$target}) {
+    // 초기화 데이터
     const intialState = getItem('todos', [
         {
             id: 1,
-            text: "공부하기"
+            text: "공부하기",
+            checked: true
         },
         {
             id: 2,
-            text: "공부하기"
+            text: "공부하기",
+            checked: true
         }
     ]);
 
+    // 등록 고유 id
     let lastId = intialState[intialState.length - 1].id;
 
     // 할일 등록 메서드
@@ -23,6 +27,7 @@ export default function App({$target}) {
             {
                 id: lastId + 1,
                 text,
+                checked: false
             }
         ])
         lastId++;
@@ -34,6 +39,16 @@ export default function App({$target}) {
     const onDelete = (id) => {
         const nextState = todoList.state.filter(todo => todo.id !== id);
         todoList.setState(nextState);
+        setItem('todos', JSON.stringify(nextState));
+    }
+
+    // 할일 체크 여부
+    const onToggle = (id) => {
+        const nextState = todoList.state.map(todo => 
+            todo.id === id ? {...todo, checked: !todo.checked} : todo
+        );
+        todoList.setState(nextState);
+        setItem('todos', JSON.stringify(nextState));
     }
 
     new Header({$target});
@@ -46,7 +61,8 @@ export default function App({$target}) {
     const todoList = new TodoList({
         $target, 
         intialState,
-        onDelete
+        onDelete,
+        onToggle
     })
 
 }
